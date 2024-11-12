057d736dccca53467f95a420211486fbfca0cf62
root <root@diego-laptop>
2024-11-12 02:40:48 -0600

    Migrate to TS

---
+++ ui-plugin/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <span>Hello world!</span>
+    </div>
+  );
+}
+
+export default App;
