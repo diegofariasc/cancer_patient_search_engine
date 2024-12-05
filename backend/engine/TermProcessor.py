5f9e8e2f6cd4b616cada856656325437196387eb
root <root@diego-laptop>
2024-12-04 21:49:47 -0600

    Allow only terms with minimum length

--- backend/engine/TermProcessor.py
+++ backend/engine/TermProcessor.py
@@ -16,14 +16,29 @@ class TermProcessor:
         self.__puntuation_set = build_punctuation_set()
         self.__stemmer_collection = build_stemmer_collection()
 
-    def get_terms(self, text: str, language: DocumentLanguage = None) -> list[str]:
+    def get_term_frequencies(
+        self, text: str, language: DocumentLanguage = None
+    ) -> list[str]:
         text = text.lower()
         tokens = word_tokenize(text)
         language = get_text_language(text, language)
-        terms = [
-            self.__stemmer_collection[language].stem(term)
-            for term in tokens
-            if not is_stopword(term, language, self.__stopword_set_collection)
-            and term not in self.__puntuation_set
-        ]
+
+        terms = {}
+
+        for term in tokens:
+            if (
+                not term.isalpha()
+                or len(term) < 3
+                or is_stopword(term, language, self.__stopword_set_collection)
+                or term in self.__puntuation_set
+            ):
+                continue
+
+            stemmed_term = self.__stemmer_collection[language].stem(term)
+
+            if stemmed_term in terms:
+                terms[stemmed_term] += 1
+            else:
+                terms[stemmed_term] = 1
+
         return terms
